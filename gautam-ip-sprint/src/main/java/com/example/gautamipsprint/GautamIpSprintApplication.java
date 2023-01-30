8233eb1c3d6f689da1e0c313c9cd8bfb77e823c5
gnair <gnair@marketaxess.com>
2023-01-30 15:02:56 -0500

    base

---
+++ gautam-ip-sprint/src/main/java/com/example/gautamipsprint/GautamIpSprintApplication.java
@@ -0,0 +1,13 @@
+package com.example.gautamipsprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GautamIpSprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GautamIpSprintApplication.class, args);
+	}
+
+}
---
+++ gautam-ip-sprint/src/test/java/com/example/gautamipsprint/GautamIpSprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gautamipsprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GautamIpSprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
